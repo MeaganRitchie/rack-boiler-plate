2d1a12300d68fbb24d757ed8cba322586278c493
MeaganRitchie <meaganritchie@gmail.com>
2021-05-11 14:13:15 -0600

    Started seeding flashcard letters and images

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,10 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+
+    if req.path.match(/cards/) && req.get?
+      return [200, {'Content-Type' => 'application/json'}, [Card.all.to_json]]
+    end
   end
 
 end
---
+++ db/seeds.rb
@@ -0,0 +1,30 @@
+Card.destroy_all
+
+Card.create(
+  [
+    {
+      letter:"a",
+      url:"https://www.pinclipart.com/picdir/middle/569-5697320_transparent-apple-clip-art-red-apple-clipart-png.png"
+    },
+    {
+      letter:"b",
+      url:"https://cliparting.com/wp-content/uploads/2016/09/Cute-bus-clipart-kid-2.png"
+    },
+    {
+      letter:"c",
+      url:"https://www.vippng.com/png/detail/21-212690_free-download-high-quality-cartoon-cat-png-transparent.png"
+    },
+    {
+      letter:"s",
+      url:"https://www.pinclipart.com/picdir/big/4-40775_jaguar-pre-style-ergo-scissor-clip-art-purple.png"
+    },
+    {
+      letter:"t",
+      url:"https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/845879/coal-train-clipart-md.png"
+    },
+    {
+      letter:"z",
+      url:"https://toppng.com/uploads/preview/baby-zebra-clipart-ba-zebra-clipart-zebra-i-love-animals-baby-zebra-clipart-115628585807kwannfvqb.png"
+    }
+  ]
+)
